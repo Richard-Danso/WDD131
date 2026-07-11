78dfb79e1956e209b0babe7e306f6d99448fe129
Richard-Danso <kwesidollar25@gmail.com>
2026-07-11 21:26:22 +0000

    change

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Dynamically populate the current year for the copyright notice
+document.getElementById("current-year").textContent = new Date().getFullYear();
+
+// Dynamically populate the document's last modified date and time
+document.getElementById("last-modified").textContent = document.lastModified;
